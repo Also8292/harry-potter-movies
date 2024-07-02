c4930905be1483a35aa3b616f0d150a172ab3cd5
AliouSow <sowalils1992@gmail.com>
2024-07-02 19:34:12 +0200

    currency pipe

--- src/app/shared/custom-currency.pipe.ts
+++ src/app/shared/custom-currency.pipe.ts
@@ -6,14 +6,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CustomCurrencyPipe implements PipeTransform {
 
-  transform(value: string): string {
+  transform(value: string, unite = 'million'): string {
     if (!value) return '';
     let formatedBudget = '';
     const range = value.split('-');
     if (range.length === 1) {
-      formatedBudget = `$${range[0]} million`;
+      formatedBudget = `$${range[0]} ${unite}`;
     } else {
-      formatedBudget = `$${range[0]}-${range[1]} million`;
+      formatedBudget = `$${range[0]}-${range[1]} ${unite}`;
     }
 
     return formatedBudget;
