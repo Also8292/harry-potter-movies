ae9b2dc0a9a0463bddc29fd3b57e0bbacc55e8e2
AliouSow <sowalils1992@gmail.com>
2024-07-04 00:27:31 +0200

    Step3- details

--- src/app/details-movie/details-movie.component.ts
+++ src/app/details-movie/details-movie.component.ts
@@ -1,7 +1,13 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnDestroy, OnInit, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { MatTableModule } from '@angular/material/table';
+import { CustomCurrencyPipe } from '../shared/custom-currency.pipe';
+import { DurationPipe } from '../shared/duration.pipe';
 
 export interface MovieDetailsModel {
   id: string,
@@ -16,10 +22,16 @@ export interface MovieDetailsModel {
   summary: string
 }
 
+export interface DetailsRow {
+  label: string,
+  field: string,
+  formatter?: string
+};
+
 @Component({
   selector: 'app-details-movie',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, MatCardModule, MatIconModule, RouterModule, MatGridListModule, MatTableModule, CustomCurrencyPipe, DurationPipe],
   providers: [HttpClientModule, HttpClient],
   templateUrl: './details-movie.component.html',
   styleUrl: './details-movie.component.css'
@@ -29,14 +41,41 @@ export class DetailsMovieComponent implements OnInit, OnDestroy {
   private httpClient = inject(HttpClient);
   private route = inject(ActivatedRoute);
   private subs?: Subscription;
+  movie!: MovieDetailsModel;
+
+  detailsRows: DetailsRow[] = [
+    {
+      label: 'Box office',
+      field: 'box_office',
+      formatter: 'customCurrency'
+    },
+    {
+      label: 'Budget',
+      field: 'budget',
+      formatter: 'customCurrency'
+    },
+    {
+      label: 'Duration',
+      field: 'duration',
+      formatter: 'duration'
+    },
+    {
+      label: 'Producers',
+      field: 'producers'
+    },
+    {
+      label: 'Cinematographers',
+      field: 'cinematographers'
+    },
+  ];
 
   ngOnInit(): void {
-    this.subs = this.getListOfMovies(this.route.snapshot.params['movieId']).subscribe(movie => {
-      console.log('MOVIE : ', movie);
+    this.subs = this.getMovieDetails(this.route.snapshot.params['movieId']).subscribe(movie => {
+      this.movie = movie;
     });
   }
 
-  getListOfMovies(movieId: string): Observable<MovieDetailsModel> {
+  getMovieDetails(movieId: string): Observable<MovieDetailsModel> {
     return this.httpClient.get<MovieDetailsModel>(`/movies/${movieId}`);
   }
 
